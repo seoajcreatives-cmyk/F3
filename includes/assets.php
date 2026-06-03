9c943c87c170f2d0a768297fb521d260f2d7db96
seoajcreatives-cmyk <seoajcreatives@gmail.com>
2026-06-03 15:53:43 -0500

    Try commenting on trackers

--- includes/assets.php
+++ includes/assets.php
@@ -7,31 +7,31 @@ $base_path = (basename(dirname($_SERVER['SCRIPT_NAME'])) === 'locations') ? '../
 <meta name="author" content="Aj Creative Studios">
 
 <!-- Google tag (gtag.js) -->
-<script defer src="https://www.googletagmanager.com/gtag/js?id=G-PFCD90JNJ7"></script>
+<!-- <script defer src="https://www.googletagmanager.com/gtag/js?id=G-PFCD90JNJ7"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-PFCD90JNJ7');
-</script>
+</script> -->
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N5BNJZJB');</script>
+})(window,document,'script','dataLayer','GTM-N5BNJZJB');</script> -->
 <!-- End Google Tag Manager -->
 
 <!-- Microsoft Clarity -->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.defer=true;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "tqlfr5xzsw");
-</script>
+</script> -->
 <!-- End Microsoft Clarity -->
 
 <link rel="shortcut icon" href="https://f3constructionny.com/media/iconified/favicon.ico" type="image/x-icon" />
